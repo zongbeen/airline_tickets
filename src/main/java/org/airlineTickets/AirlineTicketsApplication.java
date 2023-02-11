faa4806e0eb1f674b733f7a9e9dd26e7ccafcc61
zongbeen <hjb825@naver.com>
2023-02-11 16:39:05 +0900

    Initial commit

---
+++ src/main/java/org/airlineTickets/AirlineTicketsApplication.java
@@ -0,0 +1,13 @@
+package org.airlineTickets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirlineTicketsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AirlineTicketsApplication.class, args);
+    }
+
+}
